cc2e577ade63b782858f25d0005c2bd760d18918
masluk <madis.luik@outlook.com>
2018-04-17 16:10:24 +0300

    muudatus

--- page-templates/api.php
+++ page-templates/api.php
@@ -17,49 +17,55 @@ use Automattic\WooCommerce\Client;
 use Automattic\WooCommerce\HttpClient\HttpClientException;
 
 
-$url = 'https://mars.excellent.ee:2888/api/1/invc?fields=code,name,group,uprice1';
+$url = 'https://mars.excellent.ee:2888/api/1/invc?fields=code,name,group,uprice1,weight';
 $context = stream_context_create(array(
     'http' => array(
         'header'  => "Authorization: Basic " . base64_encode("$username:$password"),
-        'timeout' => .5
+        'timeout' => 2.0
     )
 ));
-$raw_data = file_get_contents($url, false, $context);
-$data = json_decode($raw_data);
-
-var_dump($raw_data);
+$data = file_get_contents($url, false, $context);
+$raw_data = new SimpleXMLElement($data);
 
 $newArticles = array();
-//while(!feof($raw_data)){
-//    $custInfo=(fgetcsv($raw_data));
-//    $company=$custInfo[0];
-//    $fname=$custInfo[1];
-//    $lname=$custInfo[2];
-//    $email=$custInfo[3];
-//
-//    $new_article = [
-//        'name' => 'Premium Quality',
-//        'type' => 'simple',
-//        'regular_price' => '21.99',
-//        'description' => '',
-//        'short_description' => '',
-//        'categories' => [
-//            [
-//                'id' => 9
-//            ],
-//            [
-//                'id' => 14
-//            ]
-//        ],
-//        'images' => [
-//            [
-//                'src' => '',
-//                'position' => 0
-//            ]
-//        ]
-//    ];
-//    array_push($newArticles, $new_article);
-//}
+
+foreach ($raw_data->INVc as $article) {
+    $code = (integer)$article->Code;
+    $name = (string)$article->Name;
+    $price = (string)$article->UPrice1;
+    $category = (string)$article->Group;
+    $weight = (integer)$article->Weight;
+
+    switch ($category) {
+        case "JOON":
+            $cat_id = 58;
+            break;
+        case "EHITU":
+            $cat_id = 59;
+            break;
+    }
+
+    $new_article = [
+        'name' => $name,
+        'type' => 'simple',
+        'regular_price' => $price,
+        'description' => 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.',
+        'short_description' => 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
+        'weight' => $weight,
+        'categories' => [
+            [
+                'id' => $cat_id
+            ]
+        ],
+        'images' => [
+            [
+                'src' => 'http://demo.woothemes.com/woocommerce/wp-content/uploads/sites/56/2013/06/T_2_front.jpg',
+                'position' => 0
+            ]
+        ]
+    ];
+    array_push($newArticles, $new_article);
+}
 
 //var_dump($newArticles);
 
@@ -73,14 +79,14 @@ $woocommerce = new Client(
     ]
 );
 
-//foreach ($newArticles as $customer) {
-//    try {
-//        $results = $woocommerce->post('customers', $customer);
-//        echo "<p>Customer ".$results->email." was created.</p>";
-//
-//    } catch (HttpClientException $e) {
-//        echo "<p>".$e->getMessage()."</p>"; // Error message.
-//        $e->getRequest(); // Last request data.
-//        $e->getResponse(); // Last response data.
-//    }
-//}
+foreach ($newArticles as $product) {
+    try {
+        $results = $woocommerce->post('products', $product);
+        echo "<p>Toode lisati.</p>";
+
+    } catch (HttpClientException $e) {
+        echo "<p>".$e->getMessage()."</p>"; // Error message.
+        $e->getRequest(); // Last request data.
+        $e->getResponse(); // Last response data.
+    }
+}
